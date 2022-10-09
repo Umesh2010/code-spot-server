892b3e1d0ab5ee94cdadaffdc9618affac4728e1
Umesh2010 <umesh.panchal501@gmail.com>
2022-10-09 21:49:16 +0530

    mongoose

--- server/src/db/conn.js
+++ server/src/db/conn.js
@@ -1,35 +1,35 @@
-const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI || "mongodb+srv://code-spot:UwR7OC5eskgZDxG8@cluster0.bb6aqkq.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// const { MongoClient } = require("mongodb");
+// const Db = process.env.ATLAS_URI || "mongodb+srv://code-spot:UwR7OC5eskgZDxG8@cluster0.bb6aqkq.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(Db, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-let _db;
+// let _db;
 
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db) {
-        _db = db.db("spot");
-        console.log("Successfully connected to MongoDB.");
-      }
-      return callback(err);
-    });
-  },
+// module.exports = {
+//   connectToServer: function (callback) {
+//     client.connect(function (err, db) {
+//       // Verify we got a good "db" object
+//       if (db) {
+//         _db = db.db("spot");
+//         console.log("Successfully connected to MongoDB.");
+//       }
+//       return callback(err);
+//     });
+//   },
 
-  getDb: function () {
-    return _db;
-  },
-};
+//   getDb: function () {
+//     return _db;
+//   },
+// };
 
 // mongooes connection success
 
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const db = {};
-// db.mongoose = mongoose;
-// db.url = process.env.ATLAS_URI || "mongodb+srv://code-spot:UwR7OC5eskgZDxG8@cluster0.bb6aqkq.mongodb.net/?retryWrites=true&w=majority";
+const db = {};
+db.mongoose = mongoose;
+db.url = process.env.ATLAS_URI || "mongodb+srv://code-spot:UwR7OC5eskgZDxG8@cluster0.bb6aqkq.mongodb.net/?retryWrites=true&w=majority";
 
-// module.exports = db;
+module.exports = db;
