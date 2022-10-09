150bf1b52c004d6b2186063a95402d452c2e65ed
Umesh2010 <umesh.panchal501@gmail.com>
2022-10-09 21:07:45 +0530

    test 2

--- server/src/server.js
+++ server/src/server.js
@@ -2,23 +2,37 @@ const express = require("express");
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
-const dbo = require("./db/conn");
+// const dbo = require("./db/conn");
+const db = require("./db/conn");
 
-app.get('/', async (req, res) => {
-    let db_connect = dbo.getDb();
-    const peers = await db_connect.collection("peers").find().toArray();
-    res.json({
-        message: "Run Success",
-        data: peers
-    });
+
+// app.get('/', async (req, res) => {
+    // let db_connect = dbo.getDb();
+    // const peers = await db_connect.collection("peers").find().toArray();
+    // res.json({
+    //     message: "Run Success",
+    //     data: peers
+    // });    
+// });
+
+app.get("/", async (req, res) => {
+    db.mongoose.connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => {
+            res.json({ message: "MongoDB Connection Successfully" })
+        }).catch(err => {
+            res.json({ message: "MongoDB Connection Fail", error: err })
+        });
 });
 
 app.use(require("./routes/rooms"));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    dbo.connectToServer(function (err) {
-        if (err) console.error(err);
-    });
+    // dbo.connectToServer(function (err) {
+    //     if (err) console.error(err);
+    // });
     console.log('Server is running on http://localhost:' + PORT);
 });
